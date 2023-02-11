bf69a0aff7f2456cd3703647476ffbe4405b5e7a
RadosRosic <rados.rosic@gmail.com>
2023-02-11 18:04:47 +0100

    click on genre redirects to landing page with genre filter selected

--- src/components/LandingPage/Genres.jsx
+++ src/components/LandingPage/Genres.jsx
@@ -1,14 +1,30 @@
-import { Avatar, Box } from "@mui/material";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { Avatar, Box } from "@mui/material";
+import { selectGenre } from "../../store/allShowsSlice";
 
 const genresBoxSpecs = { display: "flex", flexWrap: "wrap" };
-const genresSpecs = { width: "fit-content", p: 1, m: 0.5 };
+const genresSpecs = { width: "fit-content", p: 1, m: 0.5, cursor: "pointer" };
 
 const Genres = ({ genres }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const seeOtherShows = (genre) => {
+    dispatch(selectGenre(genre));
+    navigate("/");
+  };
+
   return (
     <Box sx={genresBoxSpecs}>
       {genres?.map((genre) => (
-        <Avatar variant="square" sx={genresSpecs} key={genre}>
+        <Avatar
+          variant="square"
+          sx={genresSpecs}
+          key={genre}
+          onClick={() => seeOtherShows(genre)}
+        >
           {genre}
         </Avatar>
       ))}
